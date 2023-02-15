e0b98cfbeb1ab96681e32c34bebcb9026c5adce8
Christopher Maltez <cflux.maltez@live.com>
2023-02-15 10:57:47 -0800

    separating out app.py and troubling shooting blueprints integration

---
+++ part_3/warbler/__init__.py
@@ -0,0 +1,97 @@
+import os
+from dotenv import load_dotenv
+
+from warbler.root import root_views
+
+
+from flask import (
+    Flask, render_template, request, flash, redirect, session, g, abort, Blueprint
+)
+from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
+
+from forms import (
+    UserAddForm, UserEditForm, LoginForm, MessageForm, CSRFProtection,
+)
+from models import (
+    db, connect_db, User, Message, DEFAULT_IMAGE_URL, DEFAULT_HEADER_IMAGE_URL)
+
+load_dotenv()
+
+CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+
+app.register_blueprint(root_views)
+
+
+# Get DB_URI from environ variable (useful for production/testing) or,
+# if not set there, use development local db.
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+
+
+##############################################################################
+# Before/after request and error handler
+
+
+@app.before_request
+def add_user_to_g():
+    """If we're logged in, add curr user to Flask global."""
+
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
+
+    else:
+        g.user = None
+
+
+@app.before_request
+def add_csrf_only_form():
+    """Add a CSRF-only form so that every route can use it."""
+
+    g.csrf_form = CSRFProtection()
+
+
+def do_login(user):
+    """Log in user."""
+
+    session[CURR_USER_KEY] = user.id
+
+
+def do_logout():
+    """Log out user."""
+
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """404 NOT FOUND page."""
+
+    return render_template('404.html'), 404
+
+
+@app.after_request
+def add_header(response):
+    """Add non-caching headers on every request."""
+
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
+    response.cache_control.no_store = True
+    return response
+
+
+
+
+
+
+
+
---
+++ part_3/warbler/root/views.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template
+
+root_views = Blueprint('root_views', __name__,
+                        template_folder='templates')
+
+##############################################################################
+# Homepage
+
+
+@root_views.get('/')
+def homepage():
+    """Show homepage:
+
+    - anon users: no messages
+    - logged in: 100 most recent messages of self & followed_users
+    """
+
+    if g.user:
+        following_ids = [f.id for f in g.user.following] + [g.user.id]
+
+        messages = (Message
+                    .query
+                    .filter(Message.user_id.in_(following_ids))
+                    .order_by(Message.timestamp.desc())
+                    .limit(100)
+                    .all())
+
+        return render_template('home.html', messages=messages)
+
+    else:
+        return render_template('home-anon.html')
